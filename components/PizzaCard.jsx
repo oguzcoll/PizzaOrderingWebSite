d345ac500481134865907b2705a813315cdc71ee
Oguz Colak <oguzcolak2000@gmail.com>
2023-03-06 15:30:52 +0300

    Next.js REST API with mongoDb is completed

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import styles from "../styles/PizzaCard.module.css";
+import Link from "next/link";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="250" height="250" />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.99</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, minus?
-      </p>
+      <Link href={`/product/${pizza._id}`} passHref>
+        <Image src={pizza.img} alt="" width="250" height="250" />
+      </Link>
+      <h1 className={styles.title}>{pizza.title}</h1>
+      <span className={styles.price}>${pizza.prices[0]}</span>
+      <p className={styles.desc}>{pizza.desc}</p>
     </div>
   );
 };
